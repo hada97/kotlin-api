a6babcc011ab2bf4987dc53041a317bb8b0fda62
Adailson <adailson65@gmail.com>
2024-09-26 22:05:24 -0300

    add PUT

--- forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.model.dto.NovoTopicoForm
 import br.com.alura.forum.service.TopicoService
@@ -7,6 +8,7 @@ import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -26,8 +28,13 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
-        service.cadastrar(dto)
+    fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) {
+        service.cadastrar(form)
+    }
+
+    @PutMapping
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) {
+        service.atualizar(form)
     }
 
 }
--- forum/src/main/kotlin/br/com/alura/forum/mapper/TopicoViewMapper.kt
+++ forum/src/main/kotlin/br/com/alura/forum/mapper/TopicoViewMapper.kt
@@ -2,8 +2,6 @@ package br.com.alura.forum.mapper
 
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.service.CursoService
-import br.com.alura.forum.service.UsuarioService
 import org.springframework.stereotype.Component
 
 @Component
